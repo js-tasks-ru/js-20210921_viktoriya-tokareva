e5d79e810a78987e7c2f538e820994bafbdae52f
Viktoriya Tokareva <coke_alice@mail.ru>
2021-09-25 15:27:53 +0500

    tasks of the 1st module

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,20 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let sortedArr = arr.slice();
+    if (param == 'asc') {
+        if (/[а-я]/i.test(sortedArr[0])) {
+            return sortedArr.sort((a, b) => a.localeCompare(b, 'ru-u-kf-upper'));
+        }
+        if (/[a-z]/i.test(sortedArr[0])) {
+            return sortedArr.sort((a, b) => a.localeCompare(b, 'en-u-kf-upper'));
+        }
+    } else {
+        if (/[а-я]/i.test(sortedArr[0])) {
+            return sortedArr.sort((a, b) => b.localeCompare(a, 'ru-u-kf-lower'));
+        }
+        if (/[a-z]/i.test(sortedArr[0])) {
+            return sortedArr.sort((a, b) => b.localeCompare(a, 'en-u-kf-lower'));
+        }
+    }
 }
